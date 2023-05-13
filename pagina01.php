13d1578901c3afb19c488f8db2e90daf9ef3c2b1
alinemn <aline.ams91@gmail.com>
2023-05-12 22:21:28 -0300

    inserção de cidade

--- pagina01.php
+++ pagina01.php
@@ -1,4 +1,20 @@
 <?php
     echo 'Aline Mendes';
     echo 'Jardim Roseira';
+
+    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
 ?>
+
+
+git add .
+git commit -m "nomedapastadoprojeto"
+git push origin head:main (para subir as modificações)
+git pull origin main (para baixar o projeto)
+
+git pull origin HEAD:main --allow-unrelated-histories (quando o token não funcionar)
+
+na primeira vez
+fazer conta no github
+baixar o git
+no prompt:
+git init
